4261dab617c800f8386c80d117cb2ebb72374cec
jijid13 <jijid13@hotmail.com>
2017-03-21 17:34:56 +0100

    exclude Java Stored
    exclude Java Stored Procedure for check object compilation

--- checkInvalidObjects.sql
+++ checkInvalidObjects.sql
@@ -3,7 +3,8 @@ EXEC DBMS_UTILITY.compile_schema(schema => '&1');
 SPOOL invalidObjects.out
 select count(object_name) || ' invalid objects'
 from dba_objects
-where status = 'INVALID';
+where status = 'INVALID'
+AND OBJECT_TYPE != 'JAVA CLASS';
 
 SELECT owner,
        object_type,
@@ -11,6 +12,7 @@ SELECT owner,
        status
 FROM   dba_objects
 WHERE  status = 'INVALID'
+AND OBJECT_TYPE != 'JAVA CLASS'
 ORDER BY owner, object_type, object_name;
 SPOOL OFF;
 exit;
