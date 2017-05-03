f643ef2fac27f19162be6c3c1d664da26b623f42
jijid13 <jijid13@hotmail.com>
2017-05-03 09:30:00 +0200

    filter out some object like java stored procedure

--- checkInvalidObjects.sql
+++ checkInvalidObjects.sql
@@ -4,7 +4,8 @@ SPOOL invalidObjects.out
 select count(object_name) || ' invalid objects'
 from dba_objects
 where status = 'INVALID'
-AND OBJECT_TYPE != 'JAVA CLASS';
+AND OBJECT_TYPE != 'JAVA CLASS'
+AND object_name not in ('&2');
 
 SELECT owner,
        object_type,
@@ -13,6 +14,7 @@ SELECT owner,
 FROM   dba_objects
 WHERE  status = 'INVALID'
 AND OBJECT_TYPE != 'JAVA CLASS'
+AND object_name not in ('&2')
 ORDER BY owner, object_type, object_name;
 SPOOL OFF;
 exit;
